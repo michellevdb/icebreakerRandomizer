cb4c90da8ea7a606565821ecb87dc9fbea3971de
Michelle Van Der Berg <michellecyou@gmail.com>
2020-03-11 10:43:47 -0700

    move up image

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -253,6 +253,7 @@ var questions = [
 
 ]
 
+
 function newQuestion() {
     var randomNumber = Math.floor(Math.random() * (questions.length));
     document.getElementById('question-display').innerHTML = questions[randomNumber]
